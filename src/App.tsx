690867443e1ed5a22864b33b0b808887b24bc7df
Dakudbilla <akuddev@gmail.com>
2022-08-02 08:52:06 +0000

    Fix: added styles to make footer always at bottom of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import NavbBar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { Route, Routes } from "react-router-dom";
@@ -9,19 +8,28 @@ import SearchResult from "./components/SearchResult/SearchResult";
 import { FavMovieContextProvider } from "./context";
 import Favourites from "./components/Favorites/Favorites";
 const App = () => {
-  return (
+  //style to make footer always at the bottom of page
+  const appStyle = {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+  } as React.CSSProperties;
 
+  return (
     <FavMovieContextProvider>
-      <NavbBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<Movie />} />
-        <Route path="/tv/:id" element={<Movie />} />
-        <Route path="/search" element={<SearchResult />} />
-        <Route path="/favorites" element={<Favourites />} />
-      </Routes>
-      <Footer />
+      <div style={appStyle}>
+        <NavbBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<Movie />} />
+          <Route path="/tv/:id" element={<Movie />} />
+          <Route path="/search" element={<SearchResult />} />
+          <Route path="/favorites" element={<Favourites />} />
+        </Routes>
+
+        <Footer />
+      </div>
     </FavMovieContextProvider>
   );
 };
