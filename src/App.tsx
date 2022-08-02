5caed015967a355055d7605ebd0ef7abd6c627de
Dakudbilla <akuddev@gmail.com>
2022-08-02 10:57:02 +0000

    Fix: navbar messup when  footer is moved to bottom of page

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,19 @@ import SearchResult from "./components/SearchResult/SearchResult";
 import { FavMovieContextProvider } from "./context";
 import Favourites from "./components/Favorites/Favorites";
 const App = () => {
-  //style to make footer always at the bottom of page
-  const appStyle = {
-    display: "flex",
-    flexDirection: "column",
-    height: "100vh",
-  } as React.CSSProperties;
-
   return (
     <FavMovieContextProvider>
-      <div style={appStyle}>
-        <NavbBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:id" element={<Movie />} />
-          <Route path="/tv/:id" element={<Movie />} />
-          <Route path="/search" element={<SearchResult />} />
-          <Route path="/favorites" element={<Favourites />} />
-        </Routes>
+      <NavbBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<Movie />} />
+        <Route path="/tv/:id" element={<Movie />} />
+        <Route path="/search" element={<SearchResult />} />
+        <Route path="/favorites" element={<Favourites />} />
+      </Routes>
 
-        <Footer />
-      </div>
+      <Footer />
     </FavMovieContextProvider>
   );
 };
--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,31 +1,36 @@
-import { useEffect } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { useFaveContext } from "../../context"
-import FavMediaCard from "./FavMovieCard"
-import './favorites.css'
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useFaveContext } from "../../context";
+import FavMediaCard from "./FavMovieCard";
+import "./favorites.css";
 
 const Favourites = () => {
-    const { favMedia, isUserLoggedIn } = useFaveContext()
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (!isUserLoggedIn) {
-            navigate('/')
-        }
-    }, [])
+  const { favMedia, isUserLoggedIn } = useFaveContext();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isUserLoggedIn) {
+      navigate("/");
+    }
+  }, []);
 
-
-
-    return <div className="fav-container">
-        <h1>
-            Your Favourite TV Shows and Movies
-        </h1>
-        {
-            favMedia && favMedia.length > 0 ? favMedia.map((media) => <FavMediaCard media={media} key={media.mediaId} />) :
-                <h2><em>No Favorites added, <Link to='/'>Explore Movies</Link>  </em> </h2>
-
-        }
+  return (
+    <div className="fav-container">
+      {favMedia && favMedia.length > 0 && (
+        <h1>Your Favourite TV Shows and Movies</h1>
+      )}
+      {favMedia && favMedia.length > 0 ? (
+        favMedia.map((media) => (
+          <FavMediaCard media={media} key={media.mediaId} />
+        ))
+      ) : (
+        <h2>
+          <em className="no-fav">
+            No Favorites added, <Link to="/">Explore Movies</Link>
+          </em>
+        </h2>
+      )}
     </div>
+  );
+};
 
-}
-
-export default Favourites
+export default Favourites;
